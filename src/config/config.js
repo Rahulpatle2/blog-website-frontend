00ff1f14a5f453a2656203912bbcb77f8e041df2
rahulpatle2 <rahul25ec050@satiengg.in>
2025-12-25 18:00:56 +0530

    config file updated

--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const instance = axios.create(
     {
-        // baseURL: import.meta.env.VITE_API_URL,
-        baseURL:'http://localhost:3000',
+        baseURL: import.meta.env.VITE_API_URL,
         withCredentials: true,
     }
 )
